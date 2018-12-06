ba3272b618a59cf0a6e03d8ace8d7177e212adbf
Unknown <kaa516@nyu.edu>
2018-12-06 16:09:40 -0500

    candle Lighting implemented

---
+++ solitaire/Assets/LightingScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingScript : MonoBehaviour {
+
+    public float litTimer;
+    public GameObject flamePrefab;
+
+	// Use this for initialization
+	void Start () {
+        litTimer = 0f;
+        flamePrefab.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       // litTimer -= Time.deltaTime;
+
+        if (litTimer > 0 && litTimer < 5)
+        {
+            this.flamePrefab.SetActive(true);          
+
+        }
+        else {
+            this.flamePrefab.SetActive(false);
+        }
+
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            litTimer = 5f;
+            litTimer -= Time.deltaTime;
+
+        }
+    }
+
+}
